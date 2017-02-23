18c9cb8f098a992f1fa9b698f16c9f18c78c794a
大島彩華 <1515honoka@gmail.com>
2017-02-23 17:35:52 +0900

    ScriptTest  　Ｃ＃課題提出
    要素の個数が5の、int型の配列arrayを宣言して好きな値で初期化
    配列の各要素の値を順番に表示
    for文を使い、配列の各要素の値を逆順に表示

---
+++ Assets/Test.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Use this for initialization
+    void Start()
+    {
+        //
+        int[] array = { 50, 40, 30, 20, 10 };
+
+        //
+        for (int i =0; i <5; i++)
+        {
+            Debug.Log(array[i]);
+        }
+
+        //
+        for (int i = array.Length-1; i >= 0; i--)
+        {
+            //
+          
+    
+            //
+        }
+}
+
+// Update is called once per frame
+void Update()
+    {
+
+    }
+}
